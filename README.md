2e2b82a9a971816499c219d3b1b673c72dbd30b2
dreyar <thanhhoacity2506>
2021-05-05 10:38:06 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Provide graphql query for cash on delivery payment method
+
+## Pre-Requisite
+* [Magento 2](https://github.com/magento/magento2) >= 2.3.x
+
+## Contact
+Email - ldtsoftwarevn@gmail.com
---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Developer
+ *
+ * @author      Trong Le (trongithust@gmail.com)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MW_CodGraphql',
+    __DIR__
+);
